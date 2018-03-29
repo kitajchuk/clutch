a91392928e20438e55be4290cace7a711be5e99c
kitajchuk <kitajchuk@gmail.com>
2018-03-29 10:36:45 -0700

    Install source from ProperJS/app on bootstrap if not exists

--- clutch.install.js
+++ clutch.install.js
@@ -12,6 +12,7 @@ const rootTemplatePartials = path.join( root, "template", "partials" );
 const rootNotes = path.join( root, ".notes" );
 const rootServer = path.join( root, "server" );
 const rootTasks = path.join( root, "tasks" );
+const rootSource = path.join( root, "source" );
 const rootHobo = path.join( rootNodeModules, "properjs-hobo" );
 const child_process = require( "child_process" );
 const config = require( "./clutch.config" );
@@ -23,15 +24,7 @@ const config = require( "./clutch.config" );
 
 
 
-// 1.0: Fresh `node_modules`
-console.log( "Installing node_modules..." );
-
-child_process.execSync( `rm -rf ${rootPackageLock}` );
-// child_process.execSync( `rm -rf ${rootNodeModules}` );
-child_process.execSync( "npm install" );
-
-
-// 2.0 Create sandbox
+// 1.0 Create sandbox
 console.log( "Creating sandbox..." );
 
 if ( !fs.existsSync( rootSandbox ) ) {
@@ -39,7 +32,7 @@ if ( !fs.existsSync( rootSandbox ) ) {
 }
 
 
-// 3.0 Create template partials
+// 2.0 Create template partials
 console.log( "Creating template partials..." );
 
 if ( !fs.existsSync( rootTemplatePartials ) ) {
@@ -47,7 +40,7 @@ if ( !fs.existsSync( rootTemplatePartials ) ) {
 }
 
 
-// 4.0 Create notes
+// 3.0 Create notes
 console.log( "Creating notes..." );
 
 if ( !fs.existsSync( rootNotes ) ) {
@@ -55,24 +48,40 @@ if ( !fs.existsSync( rootNotes ) ) {
 }
 
 
-// 5.0 Hobo.js build
+// 4.0 Create source
+console.log( "Creating source...?" );
+
+if ( !fs.existsSync( rootSource ) ) {
+    child_process.execSync( `./node_modules/.bin/clutch source` );
+}
+
+
+// 5.0: Fresh `node_modules`
+console.log( "Installing node_modules..." );
+
+child_process.execSync( `rm -rf ${rootPackageLock}` );
+// child_process.execSync( `rm -rf ${rootNodeModules}` );
+child_process.execSync( "npm install" );
+
+
+// 6.0 Hobo.js build
 console.log( "Building properjs-hobo..." );
 
 child_process.execSync( `cd ${rootHobo} && npm install && npm run build -- '${config.browser.hobo}'` );
 
 
-// 6.0 server install
+// 7.0 server install
 console.log( "Installing server node_modules..." );
 
 child_process.execSync( `cd ${rootServer} && npm install` );
 
 
-// 7.0 tasks install
+// 8.0 tasks install
 console.log( "Installing tasks node_modules..." );
 
 if ( fs.existsSync( rootTasks ) ) {
     child_process.execSync( `cd ${rootTasks} && npm install` );
 }
 
-// 8.0 done
+// 9.0 done
 console.log( "Done!" );

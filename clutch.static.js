e95680146d095993e20d5f0c2fface4c4cb54756
kitajchuk <kitajchuk@gmail.com>
2019-11-18 17:00:36 -0800

    static generate comment

--- clutch.static.js
+++ clutch.static.js
@@ -4,7 +4,10 @@ const stasis = require( `./server/generators/${config.api.adapter}.static` );
 const lager = require( "properjs-lager" );
 
 if ( config.static.site ) {
-    // Explicit false for static, even if the node app IS using https: true
+    // Explicit false for static!
+    // Even if the node app is using https: true and you're using https for your s3 bucket,
+    // it's necessary to set this here during the static generation because clutch uses
+    // the sandbox dev server to map a static build from the /sitemap.xml endpoint.
     config.https = false;
 
     const clean = Number( process.env.CLUTCH_CLEAN );

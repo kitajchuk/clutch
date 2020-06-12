2080c4575cc9f7d17200358630e2d3d576ac10fa
kitajchuk <bk@kitajchuk.com>
2020-06-11 20:17:23 -0700

    Update ProperJS packages and webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -5,111 +5,102 @@ const config = require( "./clutch.config" );
 const lager = require( "properjs-lager" );
 const nodeModules = "node_modules";
 const webpack = require( "webpack" );
-const autoprefixer = require( "autoprefixer" );
 const BrowserSyncPlugin = require( "browser-sync-webpack-plugin" );
 
 
 
-const webpackConfig = {
-    mode: "none",
+module.exports = ( env ) => {
+    return {
+        mode: "none",
 
 
-    devtool: "source-map",
+        devtool: "source-map",
 
 
-    plugins: [
-        new webpack.LoaderOptionsPlugin({
-            options: {
-                postcss: [autoprefixer( { browsers: ["last 2 versions"] } )]
-            }
-        }),
-        new BrowserSyncPlugin({
-            open: true,
-            host: "localhost",
-            port: config.browser.port,
-            proxy: `http://localhost:${config.express.port}`,
-            files: [
-                "template/**/*.html",
-                "template/**/*.json"
+        plugins: [
+            new BrowserSyncPlugin({
+                open: true,
+                host: "localhost",
+                port: config.browser.port,
+                proxy: `http://localhost:${config.express.port}`,
+                files: [
+                    "template/**/*.html",
+                    "template/**/*.json"
+                ]
+            })
+        ],
+
+
+        resolve: {
+            modules: [root, source, nodeModules],
+            mainFields: ["webpack", "browserify", "web", "clutch", "hobo", "main"]
+        },
+
+
+        entry: {
+            "app": path.resolve( __dirname, `source/${config.theme}/js/app.js` )
+        },
+
+
+        output: {
+            path: path.resolve( __dirname, "static/js" ),
+            filename: `app.js`
+        },
+
+
+        module: {
+            rules: [
+                {
+                    test: /src\/js\/.*\.js$/i,
+                    exclude: /node_modules/,
+                    loader: "eslint-loader",
+                    enforce: "pre",
+                    options: {
+                        emitError: true,
+                        emitWarning: false,
+                        failOnError: true,
+                        quiet: true,
+                    },
+                },
+                {
+                    test: /src\/js\/.*\.js$/i,
+                    exclude: /node_modules/,
+                    use: [
+                        {
+                            loader: "babel-loader",
+                            options: {
+                                presets: ["@babel/preset-env"],
+                            },
+                        },
+                    ],
+                },
+                {
+                    test: /(hobo|hobo.build)\.js$/i,
+                    use: ["expose-loader?hobo"],
+                },
+                {
+                    test: /\.s[ac]ss$/i,
+                    exclude: /node_modules/,
+                    use: [
+                        "file-loader?name=../css/[name].css",
+                        {
+                            loader: "sass-loader",
+                            options: {
+                                sassOptions: {
+                                    outputStyle: (env.sandbox ? "uncompressed" : "compressed"),
+                                },
+                            },
+                        },
+                    ],
+                },
+                {
+                    test: /\.svg$/i,
+                    exclude: /node_modules/,
+                    use: [
+                        "svg-inline-loader",
+                    ],
+                },
             ]
-        })
-    ],
-
-
-    resolve: {
-        modules: [root, source, nodeModules],
-        mainFields: ["webpack", "browserify", "web", "clutch", "hobo", "main"]
-    },
-
-
-    entry: {
-        "app": path.resolve( __dirname, `source/${config.theme}/js/app.js` )
-    },
-
-
-    output: {
-        path: path.resolve( __dirname, "static/js" ),
-        filename: `app.js`
-    },
-
-
-    module: {
-        rules: [
-            {
-                test: /source\/js\/.*\.js$/,
-                exclude: /node_modules|vendor/,
-                loader: "eslint-loader",
-                enforce: "pre",
-                options: {
-                    emitError: true,
-                    emitWarning: false,
-                    failOnError: true,
-                    quiet: true
-                }
-            },
-            {
-                test: /source\/js\/.*\.js$/,
-                exclude: /node_modules|vendor/,
-                use: [
-                    {
-                        loader: "babel-loader",
-                        options: {
-                            presets: ["env"]
-                        }
-                    }
-                ]
-            },
-            {
-                test: /(hobo|hobo.build)\.js$/,
-                use: ["expose-loader?hobo"]
-            },
-            {
-                test: /\.(sass|scss)$/,
-                exclude: /node_modules|vendor/,
-                use: [
-                    `file-loader?name=../css/[name].css`,
-                    "postcss-loader",
-                    {
-                        loader: "sass-loader",
-                        options: {
-                            outputStyle: (config.env.sandbox ? "uncompressed" : "compressed")
-                        }
-                    }
-                ]
-            },
-            {
-                test: /\.svg$/,
-                exclude: /node_modules/,
-                use: [
-                    "svg-inline-loader"
-                ]
-            }
-        ]
-    }
-};
-
-
-
-module.exports = () => {
-    return webpackConfig;
+        }
+    };
 };
